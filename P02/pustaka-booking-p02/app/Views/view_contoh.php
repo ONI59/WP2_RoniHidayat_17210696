cc738abb18fe10d28ab35f8ee0a955826f816ef0
ONI59 <hidayatroni882@gmail.com>
2024-03-25 08:50:16 +0700

    commit pertemuan ke 1

---
+++ P02/pustaka-booking-p02/app/Views/view_contoh.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Contoh 1</title>
+</head>
+
+<body>
+      <?php echo "<h1>Perkenalkan</h1>";
+      echo "Nama saya Roni Hidayat
+              Saya tinggal di daerah Telaga Besari
+              Hobi saya adalah Motoran";
+      ?>
+
+</body>
+
+</html>
